52fb903e07d6b74df580f4e71cf26f4d9dd76706
Daniel Edgar <aknot242@gmail.com>
2021-03-11 07:10:49 -0800

    adding header iteration

--- echo.js
+++ echo.js
@@ -1,12 +1,14 @@
-export default {echo}
+export default { echo }
 
 function echo(r) {
     var body;
-    if (typeof(r.requestBuffer) != "undefined") {
+    if (typeof (r.requestBuffer) != "undefined") {
         body = r.requestBuffer;
     } else {
         var req = `<request client="${r.variables.remote_addr}" port="${r.variables.server_port}" host="${r.variables.host}" method="${r.variables.request_method}" uri="${r.uri}" httpVersion="${r.httpVersion}">
-        <headers></headers>
+          <headers>
+        ${r.headersIn.forEach(() => { return `    <${h}>${r.headersIn[h]}</${h}>\n`; })}
+          </headers>
         </request>`;
         var res = `<response status="${r.variables.return_code}" timestamp="${r.variables.time_iso8601}"></response>`;
         body = `${req}\n${res}\n`;
